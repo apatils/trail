3e424c2f659cba05dcb49692cdae5ec50106183c
apatils <41283108+apatils@users.noreply.github.com>
2019-01-30 11:07:38 +0530

    Update trail.py

--- trail.py
+++ trail.py
@@ -1,13 +1,3 @@
-import shutil
-from sqlalchemy import Column, ForeignKey, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-import codecs
-import csv
-import sqlite3
+import os
 
-print(help(sessionmaker))
-
-shutil.move("Merged/Hello.txt","Locale/Location/Hello.txt")
+os.system('sqlplus')
